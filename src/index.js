df225ddc6b9c039b9414bad30fb424dfdec4bc7a
Vlad Gritsavka <griczavka2363@gmail.com>
2023-02-07 15:02:01 +0300

    replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.scss';
 import App from './components/app/App';
 import store, { persistor } from './redux/store/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</PersistGate>
 	</Provider>
 );
