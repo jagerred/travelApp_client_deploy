e5d83ddc3e91ae8701bcae3e0a04da24e5f86f75
Vlad Gritsavka <griczavka2363@gmail.com>
2023-03-13 15:49:05 +0300

    fix subcategories loading

--- src/redux/slices/subcategoriesSlice.js
+++ src/redux/slices/subcategoriesSlice.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 const initialState = {
 	subcategories: [],
-	subcategoriesLoadStatus: 'loading',
+	subcategoriesLoadStatus: 'idle',
 };
 
 export const fetchSubcategories = createAsyncThunk(
